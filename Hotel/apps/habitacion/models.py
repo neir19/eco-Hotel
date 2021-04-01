a28487eb0e838a3b0c4ec1515f976b856d6dc10a
Neider1910 <neim1910@gmail.com>
2021-03-31 22:31:19 -0500

    refactorizar tablas

--- Hotel/apps/habitacion/models.py
+++ Hotel/apps/habitacion/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class TipoHabitacion(models.Model):
+  nombre= models.CharField(max_length=20)
+  descripcion=models.TextField()
+
+
+
 class Habitacion(models.Model):
   numero=models.CharField(max_length=5)
   estado=models.CharField(max_length=10)
   costo= models.FloatField()
   descripcion=models.TextField()
-  fkTipo= models.CharField(max_length=30)
+  fkTipo= models.ForeignKey(TipoHabitacion,null=True, blank=True, on_delete=models.CASCADE) 
+
+  
